a81c87439b4e3cf791def8af40dfd65e0d0353d8
siahmoymajid <122416542+siahmoymajid@users.noreply.github.com>
2023-03-01 17:45:30 -0800

    added code to js

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
+var currentIndex;
+var startButton;
+var quizContainer;
+var scoreform;
+var highscore;
+var score 0;
+var questionContainerEl=document.getElementById("questioncontainer")
+
+
 //start button target
 
 //event listener on click startsthequiz
-var questionBank = [
+var quizContainer = [
   {
     prompt: "Inside which HTML element do we put the JavaScript?",
     answer: "<script>",
     options: ["<javascript>", "<js>", "scripting>"],
   },
-  {
-    prompt: "Where is the correct place to insert a JavaScript?",
-    answer: "Both the <head> section and the <body> section",
-    options: ["The <head> section", "The <body> section"],
-  },
+  
   {
     prompt: "How can you add a comment in a JavaScript?",
     answer: "//This is a comment",
@@ -19,23 +24,50 @@ var questionBank = [
   },
 ];
 
-var currentIndex;
 
-
-function startQuiz() {
-  currentIndex = 0
-  console.log("starting the quiz");
-  //start the timer
-
-  //hide our start screen
+//hide our start screen
   //show our quiz screen
   //showQuestion()
-}
+function startQuiz() {
+  const startButton = document.getElementById('start-button');
+  startButton.style.display = "none";
+  questionContainerEl.setAttribute('class","hide')
+  questionContainerEl.removeAttribute('class');
+   }
 
-function showQuestion() {
-  console.log("displaying the questions");
-  var currentQuestion = questionBank[currentIndex].prompt
-  //when they select the answer
+function showquestion() {
+  console.log("displaying the question");
+  const quizContainer = document.getElementById("quiz-container");
+   }
+
+
+  // add code here to display the quiz interface
+}
+ 
+const startButton = document.getElementById("start-button");
+//start the timer
+startButton.addEventListener('click',startQuiz)
+ function startButton() {
+  console.log ('Start button is clicked');
+
+let timeEl=document.querySelector("#time");{
+
+  }
+var timeLeft = 60;
+var timerElement = document.getElementById("timer");
+
+var timer = setInterval(function() {
+ if (timeLeft <= 0) {
+clearInterval(timer);
+alert("Time's up!");
+document.querySelector("form").submit();
+ } else {
+ timerElement.textContent = timeLeft;
+timeLeft--;
+ }
+ }, 1000);
+
+
 
   // target the elemtn I want
   //change the text content to the value that I need it to be
@@ -43,8 +75,17 @@ function showQuestion() {
   //append them where I need to append them
 }
 
-function checkTheAnswer() {
-  // going to check if it's right or wrong
+
+
+
+  quizQuestions.forEach((question, index) => {
+    const questionElem = document.createElement("h2");
+    questionElem.textContent = `Question ${index + 1}: ${question.question}`;
+    quizContainer.appendChild(questionElem);
+    
+    
+
+// going to check if it's right or wrong
   //if it's wrong, deduct from secondsLeft or timeLeft
   //if(it's wrong){
   //   substract time
@@ -55,6 +96,23 @@ function checkTheAnswer() {
   // } else{
   //   gameOver()
   // }
+    question.options.forEach((option, optionIndex) => {
+      const optionElem = document.createElement("button");
+      optionElem.textContent = option;
+      optionElem.addEventListener("click", () => {
+        if (optionIndex === question.answer) {
+          alert("Correct!");
+        } else {
+          alert("Incorrect!");
+        }
+      });
+      quizContainer.appendChild(optionElem);
+    });
+  });
+}
+
+
+  
 }
 
 function gameOver() {
@@ -64,6 +122,33 @@ function gameOver() {
   // need to have an event listener
 }
 
-function initialsSubmitted() {
+
   //save the initials and our score to Local Storage
+const scoreForm = document.getElementById("score-form");
+scoreForm.addEventListener("submit", event => {
+  event.preventDefault();
+
+  const initials = document.getElementById("initials").value;
+  const score = calculateScore(); // add code here to calculate the user's score
+
+  let highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+  highScores.push({ initials, score });
+  highScores.sort((a, b) => b.score - a.score);
+  highScores.splice(5); // keep only the top 5 scores
+
+  localStorage.setItem("highScores", JSON.stringify(highScores));
+});
+
+function displayHighScores() {
+  const highScoresList = document.getElementById("high-scores");
+  highScoresList.innerHTML = "";
+
+  const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+  highScores.forEach(score => {
+    const scoreElem = document.createElement("li");
+    scoreElem.textContent = `${score.initials}: ${score.score}`;
+    highScoresList.appendChild(scoreElem);
+  });
 }
